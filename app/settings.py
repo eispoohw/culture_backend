50d4ce310efe9661c19ffa73f13e111c5250b138
eispoohw <darina.kormanovskaya@gmail.com>
2024-05-13 14:14:45 +0300

    updates cors

--- app/settings.py
+++ app/settings.py
@@ -34,6 +34,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "app.urls"
